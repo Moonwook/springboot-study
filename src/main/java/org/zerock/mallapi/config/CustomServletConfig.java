6cda23ddd694d81922da3cc469350f7a484fadeb
hi94028 <hi94028@gmail.com>
2024-07-02 22:41:14 +0900

    chap07

--- src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -2,7 +2,7 @@ package org.zerock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
 
@@ -15,14 +15,14 @@ public class CustomServletConfig implements WebMvcConfigurer{
     registry.addFormatter(new LocalDateFormatter());
   }
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry){
+  // @Override
+  // public void addCorsMappings(CorsRegistry registry){
 
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .maxAge(300)
-        .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
-  }
+  //   registry.addMapping("/**")
+  //       .allowedOrigins("*")
+  //       .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+  //       .maxAge(300)
+  //       .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+  // }
 
 }
